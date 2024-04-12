e22d2ca5224842d86b30e9745c137f979e3e502b
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-04-12 10:57:11 +0200

    adds exception manager

--- src/main/java/kaem0n/u5w2d5/exceptions/BadRequestException.java
+++ src/main/java/kaem0n/u5w2d5/exceptions/BadRequestException.java
@@ -1,9 +1,11 @@
 package kaem0n.u5w2d5.exceptions;
 
+import lombok.Getter;
 import org.springframework.validation.ObjectError;
 
 import java.util.List;
 
+@Getter
 public class BadRequestException extends RuntimeException {
     private List<ObjectError> errorList;
 
